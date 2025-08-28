2f88a5b0daefc8a59a91ff82c9795cfaea704f47
Amrit-raj50 <amrit.raj.cg@gmail.com>
2025-08-28 12:21:55 +0530

    Update user-memory.c

--- user-memory.c
+++ user-memory.c
@@ -5,4 +5,8 @@ printf("Enter first no:");
 scanf("%d",&no1);
 printf("Enter second no:");
 scanf("%d",&no2);
+  if(no1 > no2)
+    printf("%d is greater number.",no1);
+  else
+    printf("%d is greater number.",no2);
 return 0;}
